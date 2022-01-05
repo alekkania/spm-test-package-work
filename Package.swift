4346dfe79045aad1eb3ac08b148e316f93abc6e9
Aleksander Kania <aleksander.kania@dazn.com>
2022-01-05 12:59:09 +0100

    Add package firebase2

--- Package.swift
+++ Package.swift
@@ -22,7 +22,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ExecLibrary",
-            dependencies: []),
+            dependencies: [.product(name: "FirebaseFirestore",
+                                    package: "Firebase"),
+                           .product(name: "FirebaseAuth",
+                                    package: "Firebase")]),
         .testTarget(
             name: "ExecLibraryTests",
             dependencies: ["ExecLibrary"]),
