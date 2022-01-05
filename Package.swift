862512a024c480bc3f40356ef6b4e490a500e8dc
Aleksander Kania <aleksander.kania@dazn.com>
2022-01-05 13:15:01 +0100

    Add package firebase54

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ExecLibrary",
+    platforms: [.iOS(.v12), .tvOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
