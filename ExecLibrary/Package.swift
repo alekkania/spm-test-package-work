583e1fa26b5ad863a1fa2ce044a8f814c2fd4343
Aleksander Kania <aleksander.kania@dazn.com>
2022-01-05 12:03:39 +0100

    Add package firebase2

--- ExecLibrary/Package.swift
+++ ExecLibrary/Package.swift
@@ -9,18 +9,13 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ExecLibrary",
-            targets: ["ExecLibrary"],
-            
-            dependencies: [
-                .package(name: "Firebase",
-                         url: "https://github.com/firebase/firebase-ios-sdk.git",
-                         .exact("8.0.0"))
-            ]),
+            targets: ["ExecLibrary"])
         
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "Firebase",
+                 url: "https://github.com/firebase/firebase-ios-sdk.git",
+                 .exact("8.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
